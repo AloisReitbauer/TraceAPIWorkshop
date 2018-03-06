bb0804dbe4eae158a4cdf59ea875ffd50c796a47
Alois Reitbauer <alois@reitbauer.cc>
2018-03-06 12:27:41 +0100

    boilerplate for Java template

---
+++ examples/java/template/src/template/TraceExample.java
@@ -0,0 +1,13 @@
+package template;
+
+/**
+ * Single self contained example for a distributed micro application to demonstrate
+ * how different tracing APIs instrument code 
+ * @author alois.reitbauer
+ *
+ */
+public class TraceExample {
+	
+	
+
+}
